fab00218596c95a18ea2c0e53f0343c8bbb10573
G7OSTIQ <106563737+G7OSTIQ@users.noreply.github.com>
2026-05-17 23:46:24 +0200

    fixing glitch issue from web and lights
    This commit fixes the glitch and lights on the enemies

--- Assets/scripts/Effects/Glitch_effect.cs
+++ Assets/scripts/Effects/Glitch_effect.cs
@@ -25,14 +25,13 @@ public class Glitch_Effect : MonoBehaviour
 
         float timeleft = countdowntimer.gameplay_timer;
 
-        if (timeleft <= 60f)
+        if (timeleft <= 100f) //where the glitch starts playing
         {
-            float progress = Mathf.Clamp01((60f - timeleft) / 60f);
+            float progress = Mathf.Clamp01((100f - timeleft) / 100f);
             glitchvolume.weight = Mathf.SmoothStep(0f, 1f, progress);
         }
         else
         {
-         
             glitchvolume.weight = 0f;
         }
     }
